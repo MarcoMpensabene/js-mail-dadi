2ea59ef1503b91873d82295b4e10631fb4e9f2d5
MarcoMpensabene <marcompensabene@gmail.com>
2024-05-13 16:02:24 +0200

    Email : added 2 var 1 for the user email and the other for the list(array) after that used .includes and printed the result

---
+++ Mail/js/script.js
@@ -0,0 +1,15 @@
+// > Chiedi all’utente la sua email,
+// > controlla che sia nella lista di chi può accedere,
+// > stampa un messaggio appropriato sull’esito del controllo.
+
+const userEmail = prompt("Inserisci la tua email")
+const emailList = ["marco@gmail.com" ,"marco1@gmail.com" , "marco2@gmail.com" ,"marco3@gmail.com", "marco4@gmail.com", "marco5@gmail.com" ];
+
+console.log(userEmail)
+
+if (emailList.includes(userEmail)){
+    console.log("You can login")
+} else {
+    console.log("Go away from me!")
+}
+
